625c4da109c9a026bd5455e2e355347ca7b095ec
Andre Carlucci <andrecarlucci@gmail.com>
2016-02-29 22:23:10 -0300

    backup

--- SuperTddAnalyzer/CodeCompilation/IsolatedInvoker.cs
+++ SuperTddAnalyzer/CodeCompilation/IsolatedInvoker.cs
@@ -6,72 +6,59 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using TddAnalyzer.Util;
 
 namespace TddAnalyzer.CodeCompilation {
     // Provides a means of invoking an assembly in an isolated appdomain
     public static class IsolatedInvoker {
         // main Invoke method
-        public static void Invoke(string assemblyFile, string typeName, string methodName, object[] parameters) {
-            // resolve path
-            assemblyFile = Path.Combine(Environment.CurrentDirectory, assemblyFile);
-            Debug.Assert(assemblyFile != null);
-
-            // get base path
-            var appBasePath = Path.GetDirectoryName(assemblyFile);
-            Debug.Assert(appBasePath != null);
-
-            // change current directory
-            var oldDirectory = Environment.CurrentDirectory;
-            Environment.CurrentDirectory = appBasePath;
+        public static void Invoke(string assemblyFile, string typeName, string methodName, object[] parameters, IEnumerable<byte[]> references) {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+            var domainSetup = new AppDomainSetup();
+            domainSetup.ApplicationName = Guid.NewGuid().ToString();
+            domainSetup.ApplicationBase = Environment.CurrentDirectory;
+            var domain = AppDomain.CreateDomain(Guid.NewGuid().ToString(), null, domainSetup);
             try {
-                // create new app domain
-                var domain = AppDomain.CreateDomain(Guid.NewGuid().ToString(), null, appBasePath, null, false);
-                try {
-                    domain.AssemblyResolve += (s,a) => {
-                        Debug.WriteLine(a);
-                        return null;
-                    };
-                    // create instance
-                    var invoker = (InvokerHelper)domain.CreateInstanceFromAndUnwrap(Assembly.GetExecutingAssembly().Location, typeof(InvokerHelper).FullName);
-
-                    // invoke method
-                    var result = invoker.InvokeHelper(assemblyFile, typeName, methodName, parameters);
-
-                    // process result
-                    Debug.WriteLine(result);
-                }
-                catch (Exception ex) {
-                    Debug.WriteLine(ex);
-                }
-                finally {
-                    // unload app domain
-                    AppDomain.Unload(domain);
-                }
+                var invoker = (InvokerHelper)domain.CreateInstanceFromAndUnwrap(
+                    Assembly.GetExecutingAssembly().Location, 
+                    typeof(InvokerHelper).FullName);
+                invoker.InvokeHelper(assemblyFile, typeName, methodName, parameters);
             }
             finally {
-                // revert current directory
-                Environment.CurrentDirectory = oldDirectory;
+                AppDomain.Unload(domain);
             }
         }
 
-        // This helper class is instantiated in an isolated app domain
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args) {
+            //try {
+            //    var assembly = Assembly.Load(args.Name);
+            //    if (assembly != null) {
+            //        Debug.WriteLine("Loaded: " + args.Name);
+            //        return assembly;
+            //    }
+            //}
+            //catch { }
+            Debug.WriteLine("Loaded: " + args.Name);
+            var filename = args.Name.Split(',')[0];
+            //var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            //var file = Path.Combine(path, parts[0].Trim() + ".dll"));
+            var path = Path.Combine(Path.GetTempPath(), "tdd", filename + ".dll");
+            if (!File.Exists(path)) {
+                return null;
+            }
+            return Assembly.Load(path);
+        }
+
         private class InvokerHelper : MarshalByRefObject {
             // This helper function is executed in an isolated app domain
-            public object InvokeHelper(string assemblyFile, string typeName, string methodName, object[] parameters) {
-                // create an instance of the target object
+            public void InvokeHelper(string assemblyFile, string typeName, string methodName, object[] parameters) {
                 var handle = Activator.CreateInstanceFrom(assemblyFile, typeName);
-
-                // get the instance of the target object
                 var instance = handle.Unwrap();
+                instance.RunMethod(methodName);
 
-                // get the type of the target object
-                var type = instance.GetType();
-
-                // invoke the method
-                var result = type.InvokeMember(methodName, BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Instance, null, instance, parameters);
-
-                // success
-                return result;
+                //var type = instance.GetType();
+                
+                //var result = type.InvokeMember(methodName, BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Instance, null, instance, parameters);
             }
         }
     }
--- SuperTddAnalyzer/DiagnosticAnalyzer.cs
+++ SuperTddAnalyzer/DiagnosticAnalyzer.cs
@@ -12,6 +12,7 @@ using System.IO;
 using System.Diagnostics;
 using AppDomainToolkit;
 using TddAnalyzer.CodeCompilation;
+using System.Text;
 
 namespace TddAnalyzer {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
@@ -24,12 +25,12 @@ namespace TddAnalyzer {
         private const string Category = "Testing";
 
         private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
-            DiagnosticId, 
-            Title, 
-            MessageFormat, 
-            Category, 
-            DiagnosticSeverity.Warning, 
-            isEnabledByDefault: true, 
+            DiagnosticId,
+            Title,
+            MessageFormat,
+            Category,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
             description: Description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
@@ -41,7 +42,7 @@ namespace TddAnalyzer {
         public override void Initialize(AnalysisContext context) {
             context.RegisterSyntaxNodeAction(c => Analyze(c), SyntaxKind.MethodDeclaration);
         }
-        
+
         private static void Analyze(SyntaxNodeAnalysisContext context) {
             var method = (MethodDeclarationSyntax)context.Node;
             var semanticModel = context.SemanticModel;
@@ -53,6 +54,7 @@ namespace TddAnalyzer {
                 return;
             }
             var hashcode = context.SemanticModel.SyntaxTree.GetHashCode();
+            var fullMethodName = theClass.ContainingNamespace.Name + "." + theClass.Name + "." + methodSymbol.Name;
 
             var compiler = new Compiler();
             var compileResult = compiler.Compile(context.SemanticModel.Compilation);
@@ -69,101 +71,139 @@ namespace TddAnalyzer {
                 File.WriteAllBytes(Path.Combine(path, dll.Key + ".dll"), dll.Value);
             }
 
-            try {
-                Launcher.Invoke(new byte[0]);
-                //IsolatedInvoker.Invoke(Path.Combine(path, mainAssemblyName + ".dll"), theClass.Name, method.ToFullString(), null);
-
-
-                //var app = AppDomain.CreateDomain("temp");
-                //app.AssemblyResolve += (s, a) => {
-                //    //var name = a.Name;
-                //    //var index = name.IndexOf(',');
-                //    //if (index > 0) {
-                //    //    name = name.Substring(0, index);
-                //    //}
-                //    //if (!compileResult.References.ContainsKey(name)) {
-                //    //    return null;
-                //    //}
-                //    //return Assembly.Load(compileResult.References[name]);
-                //    return null;
-                //};
-
-
-                //foreach (var dll in compileResult.References) {
-                //    app.Load(dll.Value);
-                //}
-                //var testObject = app.CreateInstanceAndUnwrap(mainAssemblyName, theClass.ToString());
-                //testFramework.Run(testObject, method, semanticModel);
-                //return;
-            }
-            catch (Exception ex) {
-                if (ex.InnerException != null) {
-                    ex = ex.InnerException;
+            var errors = new StringBuilder();
+            using (var proc = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = @"nunit3-console.exe",
+                    Arguments = mainAssemblyName + ".dll --noresult --noheader --test " + fullMethodName,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                    WorkingDirectory = path
                 }
-                var message = ex.Message;
-                var diag = Diagnostic.Create(Rule, method.GetLocation(), message);
+            }) {
+                var keepMessages = false;
+                proc.Start();
+                while (!proc.StandardOutput.EndOfStream) {
+                    var line = proc.StandardOutput.ReadLine();
+                    if (line.Contains("Errors and Failures")) {
+                        keepMessages = true;
+                        continue;
+                    }
+                    if (line.Contains("Run Settings")) {
+                        break;
+                    }
+                    if (keepMessages) {
+                        errors.AppendLine(line);
+                    }
+                }
+            }
+            if (errors.Length > 0) {
+                var diag = Diagnostic.Create(Rule, method.GetLocation(), errors.ToString());
                 context.ReportDiagnostic(diag);
             }
+
+
+
+            //var analyzer = Assembly.GetExecutingAssembly();
+            //File.Copy(analyzer.Location, Path.Combine(path, Path.GetFileName(analyzer.Location)), true);
+
+            //try {
+            //    //Launcher.Invoke(new byte[0]);
+            //    IsolatedInvoker.Invoke(Path.Combine(path, mainAssemblyName + ".dll"), theClass.ToString(), methodSymbol.Name, null, compileResult.References.Select(x => x.Value));
+
+
+            //    //var app = AppDomain.CreateDomain("temp");
+            //    //app.AssemblyResolve += (s, a) => {
+            //    //    //var name = a.Name;
+            //    //    //var index = name.IndexOf(',');
+            //    //    //if (index > 0) {
+            //    //    //    name = name.Substring(0, index);
+            //    //    //}
+            //    //    //if (!compileResult.References.ContainsKey(name)) {
+            //    //    //    return null;
+            //    //    //}
+            //    //    //return Assembly.Load(compileResult.References[name]);
+            //    //    return null;
+            //    //};
+
+
+            //    //foreach (var dll in compileResult.References) {
+            //    //    app.Load(dll.Value);
+            //    //}
+            //    //var testObject = app.CreateInstanceAndUnwrap(mainAssemblyName, theClass.ToString());
+            //    //testFramework.Run(testObject, method, semanticModel);
+            //    //return;
+            //}
+            //catch (Exception ex) {
+            //    if (ex.InnerException != null) {
+            //        ex = ex.InnerException;
+            //    }
+            //    var message = ex.Message;
+            //    var diag = Diagnostic.Create(Rule, method.GetLocation(), message);
+            //    context.ReportDiagnostic(diag);
+            //}
         }
+    
 
-        //private static Type CompileTheCode(Compilation compilation, string fullClassName) {
+    //private static Type CompileTheCode(Compilation compilation, string fullClassName) {
 
-        //    foreach (var reference in compilation.References) {
-        //        var path = reference.Display;
-        //        Assembly assembly = null;
-        //        var compilationReference = reference as CompilationReference;
-        //        if (compilationReference != null) {
-        //            assembly = Compile(compilationReference.Compilation);
-        //            if (assembly == null) {
-        //                return null;
-        //            }
-        //        }
-        //        else {
-        //            assembly = Assembly.LoadFile(path);
-        //        }
-        //        var name = Path.GetFileNameWithoutExtension(path);
-        //        _references.Add(name, assembly);
-        //    }
-        //    var mainAssembly = Compile(compilation);
-        //    if (mainAssembly == null) {
-        //        return null;
-        //    }
-        //    return mainAssembly.GetType(fullClassName);
-        //}
+    //    foreach (var reference in compilation.References) {
+    //        var path = reference.Display;
+    //        Assembly assembly = null;
+    //        var compilationReference = reference as CompilationReference;
+    //        if (compilationReference != null) {
+    //            assembly = Compile(compilationReference.Compilation);
+    //            if (assembly == null) {
+    //                return null;
+    //            }
+    //        }
+    //        else {
+    //            assembly = Assembly.LoadFile(path);
+    //        }
+    //        var name = Path.GetFileNameWithoutExtension(path);
+    //        _references.Add(name, assembly);
+    //    }
+    //    var mainAssembly = Compile(compilation);
+    //    if (mainAssembly == null) {
+    //        return null;
+    //    }
+    //    return mainAssembly.GetType(fullClassName);
+    //}
 
 
-        
-        private static TestFramework ChooseTestFramework(MethodDeclarationSyntax method) {
-            foreach (var framework in TestFrameworks) {
-                if (framework.CanRun(method)) {
-                    return framework;
-                }
+
+    private static TestFramework ChooseTestFramework(MethodDeclarationSyntax method) {
+        foreach (var framework in TestFrameworks) {
+            if (framework.CanRun(method)) {
+                return framework;
             }
-            return null;
         }
-
-        private static bool IsTestMethod(MethodDeclarationSyntax method, IMethodSymbol methodSymbol) {
-            var result = false;
-            // Test if the method has any known test framework's attribute.
-            result = method.AttributeLists.HasAnyAttribute(AllTestFrameworksMethodAttributes.Value);
-
-            if (!result && methodSymbol.Name.IndexOf("Test", System.StringComparison.OrdinalIgnoreCase) >= 0) {
-                // Test if the containing class has any NUnit class attribute
-                result = methodSymbol.ContainingType.GetAttributes().Any(attribute => attribute.AttributeClass.Name == NUnitTestClassAttribute);
-
-                if (!result) {
-                    // Test if any other method in the containing class has an NUnit method attribute.
-                    result = method.Parent.DescendantNodes().Any(descendant => descendant.IsKind(SyntaxKind.MethodDeclaration) && ((MethodDeclarationSyntax)descendant).AttributeLists.HasAnyAttribute(NUnitTestMethodAttributes));
-                }
-            }
-            return result;
-        }
-
-        internal const string NUnitTestClassAttribute = "TestFixtureAttribute";
-        internal static readonly string[] MicrosoftTestMethodAttributes = new string[] { "TestMethod", "ClassInitialize", "ClassCleanup", "TestInitialize", "TestCleanup", "AssemblyInitialize", "AssemblyCleanup" };
-        internal static readonly string[] XUnitTestMethodAttributes = new string[] { "Fact", "Theory" };
-        internal static readonly string[] NUnitTestMethodAttributes = new string[] { "Test", "TestCase", "TestCaseSource", "TestFixtureSetup", "TestFixtureTeardown", "SetUp", "TearDown", "OneTimeSetUp", "OneTimeTearDown" };
-        internal static readonly System.Lazy<string[]> AllTestFrameworksMethodAttributes = new System.Lazy<string[]>(() => { return XUnitTestMethodAttributes.Concat(MicrosoftTestMethodAttributes).Concat(NUnitTestMethodAttributes).ToArray(); });
+        return null;
     }
+
+    private static bool IsTestMethod(MethodDeclarationSyntax method, IMethodSymbol methodSymbol) {
+        var result = false;
+        // Test if the method has any known test framework's attribute.
+        result = method.AttributeLists.HasAnyAttribute(AllTestFrameworksMethodAttributes.Value);
+
+        if (!result && methodSymbol.Name.IndexOf("Test", System.StringComparison.OrdinalIgnoreCase) >= 0) {
+            // Test if the containing class has any NUnit class attribute
+            result = methodSymbol.ContainingType.GetAttributes().Any(attribute => attribute.AttributeClass.Name == NUnitTestClassAttribute);
+
+            if (!result) {
+                // Test if any other method in the containing class has an NUnit method attribute.
+                result = method.Parent.DescendantNodes().Any(descendant => descendant.IsKind(SyntaxKind.MethodDeclaration) && ((MethodDeclarationSyntax)descendant).AttributeLists.HasAnyAttribute(NUnitTestMethodAttributes));
+            }
+        }
+        return result;
+    }
+
+    internal const string NUnitTestClassAttribute = "TestFixtureAttribute";
+    internal static readonly string[] MicrosoftTestMethodAttributes = new string[] { "TestMethod", "ClassInitialize", "ClassCleanup", "TestInitialize", "TestCleanup", "AssemblyInitialize", "AssemblyCleanup" };
+    internal static readonly string[] XUnitTestMethodAttributes = new string[] { "Fact", "Theory" };
+    internal static readonly string[] NUnitTestMethodAttributes = new string[] { "Test", "TestCase", "TestCaseSource", "TestFixtureSetup", "TestFixtureTeardown", "SetUp", "TearDown", "OneTimeSetUp", "OneTimeTearDown" };
+    internal static readonly System.Lazy<string[]> AllTestFrameworksMethodAttributes = new System.Lazy<string[]>(() => { return XUnitTestMethodAttributes.Concat(MicrosoftTestMethodAttributes).Concat(NUnitTestMethodAttributes).ToArray(); });
+}
 }
 
